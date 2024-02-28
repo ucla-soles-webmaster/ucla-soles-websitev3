cf46c3331b6d0a4d4b63125f14bcc765137a06f6
dianaestrada417 <estrada.diana120@gmail.com>
2024-02-28 15:32:16 -0800

    Edited mongodb connection and added user register route

--- ucla-soles-websitev3/src/index.js
+++ ucla-soles-websitev3/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './client/index.css';
-import App from './client/App';
+import App from './client/App.js';
 import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- ucla-soles-websitev3/src/server/server.js
+++ ucla-soles-websitev3/src/server/server.js
@@ -1,44 +1,51 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
-
-const userRoute = require('./routes/userRoute')
-
-
-const uri = 'mongodb+srv://ucla-soles:ucla-soles@uclawebmaster.icdi46x.mongodb.net/?retryWrites=true&w=majority';
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
-
-async function run() {
-    try {
-        await mongoose.connect(uri, clientOptions);
-        await mongoose.connection.db.admin().command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-        const User = require('./models/user'); // Adjust the path accordingly
-
-        const testUser = new User({
-            studentname: 'Test Student',
-            username: 'testuser',
-            email: 'test@example.com',
-            password: 'testpassword',
-        });
-
-        await testUser.save();
-        console.log("Test user added to the database!");
-
-    } finally {
-        await mongoose.disconnect();
-    }
-}
-
-run().catch(console.dir);
-
-// username
-// name
-// email
-// password
-// Admin
+// //import User from './models/user.js';
+// import express from "express";
+// import mongoose from 'mongoose';
+// //const mongoose = require('mongoose');
+// //const morgan = require('morgan');
+// //const bodyParser = require('body-parser');
+// //const {MongoClient}  = require("mongodb");
+// //const userRoute = require('./routes/userRoute');
 
 
-//test bank
+// const uri = 'mongodb+srv://ucla-soles:ucla-soles@uclawebmaster.icdi46x.mongodb.net/?retryWrites=true&w=majority';
+// const client = new MongoClient(uri);
+// const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+// async function run() {
+//     try {
+//         // await mongoose.connect(uri, clientOptions);
+//         // await mongoose.connection.db.admin().command({ ping: 1 });
+//         await client.connect();
+//         const db = client.db("Soles_Database");
+//         // const coll = db.collection("comets");
+//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+//         // const User = require('./models/user'); // Adjust the path accordingly
+
+//         const testUser = new User({
+//             studentname: 'Test Student',
+//             username: 'testuser',
+//             email: 'test@example.com',
+//             password: 'testpassword',
+//         });
+
+//         await testUser.save();
+//         console.log("Test user added to the database!");
+
+//     } finally {
+//         await client.close();
+//         //await mongoose.disconnect();
+//     }
+// }
+
+// run().catch(console.dir);
+
+// // username
+// // name
+// // email
+// // password
+// // Admin
+
+
+// //test bank

bd9285cc7da6c2c8e90bb3132ee24c33b859406f
dianaestrada417 <estrada.diana120@gmail.com>
2024-04-01 16:45:11 -0700

    Created rough outline of Footer and About page

---
+++ ucla-soles-websitev3/src/client/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Footer.css';
+ 
+const Footer = () => (
+    <footer className="footer">
+        <p>This is react sticky footer!!</p>
+    </footer>
+);
+ 
+export default Footer;

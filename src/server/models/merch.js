c6473e9c54df500a221a6be832ca5d170da250ac
OmarJBahena <omarbahe03@gmail.com>
2024-05-20 12:49:41 -0700

    Changed the march.js -Omar

--- src/server/models/merch.js
+++ src/server/models/merch.js
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
 
 const merchSchema = new mongoose.Schema({
-    name: String,
-    cost: String,
-    count: Number,
-    imageUrl: String,
-    stockStatus: String,
-    urlExt: String
+    name: { type: String, required: true },
+    cost: { type: String, required: true },
+    count_L: { type: String }, // Using String as per your database
+    count_M: { type: String }, // Using String as per your database
+    count_S: { type: String }, // Using String as per your database
+    count_XL: { type: String }, // Using String as per your database
+    count_XXL: { type: String }, // Using String as per your database
+    count: { type: String }, // Using String as per your database
+    imageUrl: { type: String, required: true },
+    stockStatus: { type: String, required: true },
+    urlExt: { type: String, required: true }
 });
 
-export default mongoose.model('Merch', merchSchema);
+export default mongoose.model('Merch', merchSchema, 'merch');

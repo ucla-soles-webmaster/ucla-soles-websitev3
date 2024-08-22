e7bd67d41de79f66dca8310fa1ebc424f49a77a1
diana-estrada <diana.estrada@hp.com>
2024-08-22 15:58:56 -0700

    removed node modules

--- node_modules/react-chrono/dist/components/timeline-elements/timeline-card-content/card-animations.styles.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const reveal: import("styled-components/dist/models/Keyframes").default;
-export declare const slideInFromTop: import("styled-components/dist/models/Keyframes").default;
-export declare const slideInFromLeft: import("styled-components/dist/models/Keyframes").default;
-export declare const slideFromRight: import("styled-components/dist/models/Keyframes").default;
--- node_modules/react-chrono/dist/components/timeline-elements/timeline-card-content/text-or-content.d.ts
+++
@@ -1,7 +0,0 @@
-import { TimelineContentModel } from '@models/TimelineContentModel';
-import { ForwardRefExoticComponent } from 'react';
-export type TextOrContentModel = Pick<TimelineContentModel, 'timelineContent' | 'theme' | 'detailedText'> & {
-    showMore?: boolean;
-};
-declare const getTextOrContent: (p: TextOrContentModel) => ForwardRefExoticComponent<TextOrContentModel>;
-export { getTextOrContent };

1a5ddf280c2a3dc9081854bc5fa5d09e949b2aa5
Jesus Muniz <munizzjesus04@gmail.com>
2025-04-14 18:07:47 -0700

    Made changes to technical page, added images and a placeholder button

--- src/client/pages/AlumNetwork.js
+++ src/client/pages/AlumNetwork.js
@@ -1,4 +1,5 @@
 import './AlumNetwork.css';
+import React from 'react';
 
 export default function AlumNetwork() {
     return <h1>This is the Alumni Network</h1>
--- src/client/pages/Login.js
+++ src/client/pages/Login.js
@@ -1,5 +1,6 @@
 /* global google */
 
+import React from "react";
 import {useState, useEffect} from 'react';
 import { useNavigate } from "react-router-dom";
 import {jwtDecode} from "jwt-decode";

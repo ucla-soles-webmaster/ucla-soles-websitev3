d37129cd3a4a943446b711c3558f11ef5c447448
JavierCastaneda95 <zelda86jc@gmail.com>
2025-05-05 17:19:57 -0700

    Updates to Alumni Network. Fixed CSS and added boxes to the JSX file.

--- src/client/pages/AlumNetwork.js
+++ src/client/pages/AlumNetwork.js
@@ -1,5 +1,70 @@
 import './AlumNetwork.css';
 
+
 export default function AlumNetwork() {
-    return <h1>This is the Alumni Network</h1>
+    return (<>
+        <h1 className="h1">Alumni Network</h1>
+        
+        <p className="intro">
+        The SOLES Alumni Network is a great tool for students and alumni alike. 
+        If you are a student, feel free to reach out to any of the below listed
+        alumni for career advice, job opportunities, or essentially anything! All
+        the alumni would love to hear from current students.
+        </p>
+
+        <div className="alumniBoxes">
+            <p className="h2">
+                Aerospace Engineering Alumni
+            </p>
+           
+        </div>
+
+        <div className="alumniBoxes">
+            <p className="h2">
+                Chemical Engineering + Chemistry Alumni
+            </p>
+        </div>
+
+        <div className="alumniBoxes">
+            <p className="h2">
+                Civil Engineering Alumni
+            </p>
+        </div>
+
+        <div className="alumniBoxes">
+            <p className="h2">
+                Computer Science Alumni
+            </p>
+        </div>
+
+        <div className="alumniBoxes">
+            <p className="h2">
+                Electrical Engineering Alumni
+            </p>
+        </div>
+
+        <div className="alumniBoxes">
+            <p className="h2">
+                Materials Engineering Alumni
+            </p>
+        </div>
+
+        <div className="alumniBoxes">
+            <p className="h2">
+                Mechanical Engineering Alumni
+            </p>
+        </div>
+
+        <div className="alumniBoxes">
+            <p className="h2">
+                Other Alumni
+            </p>
+        </div>
+
+
+    </>
+
+
+    );
+    
 }

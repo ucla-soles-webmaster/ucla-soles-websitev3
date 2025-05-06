1e27fa58295041293b4f854209cc95e5e84d38b2
Yanelli Raygoza <yraygoza@g.ucla.edu>
2025-05-05 17:26:32 -0700

    minor chnages

---
+++ src/client/pages/Initiatives.js
@@ -0,0 +1,16 @@
+import "./Initiatives.css";
+import React from "react";
+
+export default function Initiatives() {
+    return (
+        <div>
+            <h1> This is the Initaives Page</h1>
+            <div className="container my-5">
+                <h1 className= "Initiatives text-center">Initaitives</h1>
+            </div>
+            <div class="container">
+                <p class="h2">What We Pride In</p>
+            </div>
+        </div>
+    )
+}
